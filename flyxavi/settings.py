cb0215740c8b2ece933c460d4f13292cc6a609f3
unknown <shah@hablabs.com>
2021-11-22 02:40:25 +0530

    key

--- flyxavi/settings.py
+++ flyxavi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u@iccaym1=_j#=i624se5-h@!(-8a3&)-5u#&c#u-+&8=fa1q&'
+SECRET_KEY = 'u@iccaym1=_j#=i624se51123drf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
